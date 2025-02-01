92a589549c0de5ee5c7216ad7eb092155815f9f0
Keshab Manni <keshabmanni22@gmail.com>
2025-02-01 16:10:57 +0545

    First commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
+interface Env {}
 import { Hono } from 'hono'
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/', (ctx) => {
+  return ctx.text('Hello Hono!')
 })
 
-export default app
+app.get('/cron', (ctx) => {
+  return ctx.text('Cron job running!')
+})
+
+async function handleScheduled(event: ScheduledEvent, env: Env, ctx:ExecutionContext) {
+  console.log('Scheduled event trigerred at', new Date().toISOString())
+  console.log('Event:', event);
+  
+  console.log('Scheduled time: ', new Date(event.scheduledTime));
+  
+
+  // Create a new request to the /cron endpoint
+  const request = new Request('http://localhost/cron')
+  const response = await app.fetch(request, env)
+  
+  // Log the response from the /cron endpoint
+  console.log('Cron endpoint response:', await response.text())
+}
+
+export default {
+  fetch: app.fetch,
+  scheduled: handleScheduled
+}
